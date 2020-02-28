e6431e05e7359f96cf66a827ef24a9746a354d35
KGood93 <goodreau.kath@gmail.com>
2020-02-28 16:45:11 -0600

    Add Seed Files and update for DB Migration

--- migrations/002.do.create_circle_stats.sql
+++ migrations/002.do.create_circle_stats.sql
@@ -1,6 +1,6 @@
 CREATE TABLE circle_stats (
     date TIMESTAMP DEFAULT now() not NULL,
-    meet_id INTEGER REFERENCES circle_meetups(meetup-id),
+    meet_id INTEGER REFERENCES circle_meetups(meetup_id),
     location TEXT,
     at_count INTEGER
 );
---
+++ seeds/seed.circle_stats.sql
@@ -0,0 +1,14 @@
+INSERT INTO circle_stats (meet_id, location, at_count)
+    VALUES
+        (1, 'Rainbows End Alpacas', 32),
+        (1, 'The Loop', 40),
+        (1, 'Cream City Yarns', 63),
+        (1, 'Knitting Knook', 54),
+        (1, 'Fiberwood Studio', 28),
+        (1, 'Grafton Yarn Store', 45),
+        (1, 'Starstruck Cat Yarn Studio', 36),
+        (2, 'The Waxwing', 32),
+        (2, 'Bijou Lovely', 46),
+        (2, 'G Home Store', 34),
+        (2, 'Brick and Mortar', 52)
+

ed4c5d4961c4ed282caf03743ec14ff82e037031
KGood93 <goodreau.kath@gmail.com>
2020-03-12 17:25:41 -0500

    update Migration files

--- migrations/002.do.create_circle_stats.sql
+++ migrations/002.do.create_circle_stats.sql
@@ -2,5 +2,6 @@ CREATE TABLE circle_stats (
     date TIMESTAMP DEFAULT now() not NULL,
     meet_id INTEGER REFERENCES circle_meetups(meetup_id),
     location TEXT,
-    at_count INTEGER
+    attendance INTEGER,
+    notes TEXT
 );
--- seeds/seed.circle_meetups.sql
+++ seeds/seed.circle_meetups.sql
@@ -1,4 +1,4 @@
-INSERT INTO circle_meetups (meetup_id, meetup_name)
+INSERT INTO circle_meetups (meetup_id, meetup_name, meetup_note)
     VALUES
-        (1, 'Eastside Meetup'),
-        (2, 'Westside Meetup')
+        (1, 'Eastside Meetup', '1st Weekend of the Month'),
+        (2, 'Westside Meetup', '3rd Weekend of the Month')

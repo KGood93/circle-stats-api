22d1addf1ee93c5ef190d12d2903986a687084a9
KGood93 <goodreau.kath@gmail.com>
2020-03-12 20:07:06 -0500

    Prepare Deploy

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const {NODE_ENV} = require('./config')
+const {CLIENT_ORIGIN} = require('./config')
 
 const meetupRouter = require('./meetup/meetup-router')
 const statsRouter = require('./stats/stats-router')
@@ -15,7 +16,11 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+)
 
 app.use('/meetups', meetupRouter)
 app.use('/stats', statsRouter)

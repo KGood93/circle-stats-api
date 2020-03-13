d65bb1706abd6573766563ce0892aa81900a144d
KGood93 <goodreau.kath@gmail.com>
2020-03-12 20:08:11 -0500

    prepare deploy

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
+    PORT: process.env.PORT,
+    NODE_ENV: process.env.NODE_ENV,
     CLIENT_ORIGIN: 'https://circle-stats-app.goodreaukath.now.sh/',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://Katy:test@localhost/circle-stats',
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://Katy:test@localhost/circle-stats-test',
+    DATABASE_URL: process.env.DATABASE_URL,
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
 }

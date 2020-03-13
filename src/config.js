29e7306c1c2b6a2753bfc628dbb88e7a163c960f
KGood93 <goodreau.kath@gmail.com>
2020-03-12 21:05:11 -0500

    client origin

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT,
     NODE_ENV: process.env.NODE_ENV,
-    CLIENT_ORIGIN: 'https://circle-stats-app.goodreaukath.now.sh/',
+    CLIENT_ORIGIN: 'https://circle-stats-app.goodreaukath.now.sh',
     DATABASE_URL: process.env.DATABASE_URL,
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
 }

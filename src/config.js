54ba46a3cfcd223c9ebaa400188cb2535b6670ee
KGood93 <goodreau.kath@gmail.com>
2022-02-21 13:55:17 -0600

    update client origin address

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 module.exports = {
     PORT: process.env.PORT,
     NODE_ENV: process.env.NODE_ENV,
-    CLIENT_ORIGIN: 'https://circle-stats-app.goodreaukath.now.sh',
+    //CLIENT_ORIGIN: 'https://circle-stats-app.goodreaukath.now.sh',
+    CLIENT_ORIGIN: 'https://circle-stats-app.vercel.app/',
     DATABASE_URL: process.env.DATABASE_URL,
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
 }
